ac982abe20ec539b383daf0ee6b7d569f959172b
sagentr <66286810+sagentr@users.noreply.github.com>
2021-02-13 02:57:31 -0600

    built in piping activated

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -16,13 +16,14 @@ export class BookingComponent {
   selectedParticipants = '1'
   timeSlots = ['11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM']
   selectedTimeSlot = '11:00 AM'
+  username = '';
+  phone = '';
 
  
   onSubmit(form: NgForm) {
     console.log(form)
-   
-  }
- 
-
+    return this.bookingDetails;
   }
 
+ bookingDetails = "Your reservation has been recorded!";
+}
